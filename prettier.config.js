5445a0779c1f012748ac1639a8d1c4637cdf1b36
hanadrehman <hanad.rehman@healthifyme.com>
2021-01-23 20:22:19 +0530

    Adds lint staged husky

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 140,
+  bracketSpacing: true,
+  useTabs: false,
+  arrowParens: 'avoid',
 }
